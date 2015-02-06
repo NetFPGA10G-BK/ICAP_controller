70c4abf65527cc71ec545b710137ff1106e51ec1
Tran Chau <51100360@hcmut.edu.vn>
2015-02-06 23:23:59 +0700

    Create wptr_full.v

---
+++ wptr_full.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:26:00 02/05/2015 
+// Design Name: 
+// Module Name:    wptr_full 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module wptr_full 
+	#(
+		parameter ADDRSIZE = 4
+	)
+	(
+		output reg wfull,
+		output [ADDRSIZE-1:0] waddr,
+		output reg [ADDRSIZE :0] wptr,
+		input [ADDRSIZE :0] wq2_rptr,
+		input winc, wclk, wrst_n
+	);
+	
+	reg [ADDRSIZE:0] wbin;
+	
+	wire [ADDRSIZE:0] wgraynext, wbinnext;
+	
+	// GRAYSTYLE2 pointer
+	always @(posedge wclk or negedge wrst_n)
+		if (!wrst_n) {wbin, wptr} <= 0;
+		else {wbin, wptr} <= {wbinnext, wgraynext};
+	
+	// Memory write-address pointer (okay to use binary to address memory)
+	assign waddr = wbin[ADDRSIZE-1:0];
+	assign wbinnext = wbin + (winc & ~wfull);
+	assign wgraynext = (wbinnext>>1) ^ wbinnext;
+	//------------------------------------------------------------------
+	// Simplified version of the three necessary full-tests:
+	// assign wfull_val=((wgnext[ADDRSIZE] !=wq2_rptr[ADDRSIZE] ) &&
+	// (wgnext[ADDRSIZE-1] !=wq2_rptr[ADDRSIZE-1]) &&
+	// (wgnext[ADDRSIZE-2:0]==wq2_rptr[ADDRSIZE-2:0]));
+	//------------------------------------------------------------------
+	assign wfull_val = (wgraynext=={~wq2_rptr[ADDRSIZE:ADDRSIZE-1],wq2_rptr[ADDRSIZE-2:0]});
+	always @(posedge wclk or negedge wrst_n)
+		if (!wrst_n) wfull <= 1'b0;
+		else wfull <= wfull_val;
+
+endmodule
